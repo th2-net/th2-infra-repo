6e5471475de042d800a981243b4083ef56d59928
vakhtang.donadze <vakhtang.donadze@exactprosystems.com>
2021-04-21 13:20:08 +0400

    [TH2-1497] change logLevel into logLevelTh2 and logLevelRoot

--- src/main/java/com/exactpro/th2/infrarepo/RepositorySettings.java
+++ src/main/java/com/exactpro/th2/infrarepo/RepositorySettings.java
@@ -41,17 +41,23 @@ public class RepositorySettings {
     public static final String LOG_LEVEL_INFO = "INFO";
     public static final String LOG_LEVEL_DEBUG = "DEBUG";
 
-    private String logLevel = LOG_LEVEL_INFO;
+    private String logLevelTh2 = LOG_LEVEL_INFO;
+    private String logLevelRoot = LOG_LEVEL_INFO;
+
     private String k8sPropagation = PROPAGATE_OFF;
 
-    public String getLogLevel() {
-        return logLevel;
+    public String getLogLevelTh2() {
+        return logLevelTh2;
     }
 
-    public void setLogLevel(String logLevel) {
+    public String getLogLevelRoot() {
+        return logLevelRoot;
+    }
+
+    public void setLogLevelTh2(String logLevel) {
 
         if (logLevel == null || logLevel.equals("")) {
-            this.logLevel = LOG_LEVEL_INFO;
+            this.logLevelTh2 = LOG_LEVEL_INFO;
             return;
         }
 
@@ -59,7 +65,23 @@ public class RepositorySettings {
                 logLevel.equals(LOG_LEVEL_WARNING) ||
                 logLevel.equals(LOG_LEVEL_INFO) ||
                 logLevel.equals(LOG_LEVEL_DEBUG))
-            this.logLevel = logLevel;
+            this.logLevelTh2 = logLevel;
+        else
+            throw new IllegalArgumentException(String.format("Unknown value (%s)", logLevel));
+    }
+
+    public void setLogLevelRoot(String logLevel) {
+
+        if (logLevel == null || logLevel.equals("")) {
+            this.logLevelRoot = LOG_LEVEL_INFO;
+            return;
+        }
+
+        if (logLevel.equals(LOG_LEVEL_ERROR) ||
+                logLevel.equals(LOG_LEVEL_WARNING) ||
+                logLevel.equals(LOG_LEVEL_INFO) ||
+                logLevel.equals(LOG_LEVEL_DEBUG))
+            this.logLevelRoot = logLevel;
         else
             throw new IllegalArgumentException(String.format("Unknown value (%s)", logLevel));
     }
