7b6b94fdbc492a1d01df6340be6316a937508385
Vakhtang Donadze <vakhtang.donadze@exactprosystems.com>
2022-08-05 18:14:48 +0400

    [TH2-4051] remove bookNames section from infra manager config file

--- src/main/java/com/exactpro/th2/infrarepo/settings/BookConfig.java
+++ src/main/java/com/exactpro/th2/infrarepo/settings/BookConfig.java
@@ -16,17 +16,9 @@
 
 package com.exactpro.th2.infrarepo.settings;
 
-import java.util.List;
-
 public class BookConfig {
 
-    private List<String> bookNames;
-
     private String defaultBook;
-
-    public List<String> getBookNames() {
-        return bookNames;
-    }
 
     public String getDefaultBook() {
         return defaultBook;
